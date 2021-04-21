2053c61c39081a4694c1bea4d647d1c5b484e0f8
lisianeaguiar <lisianecampos@MacBook-Pro-de-Lisiane.local>
2021-04-21 17:48:24 -0300

    removed unused methods from service

--- src/app/modules/dashboard.service.ts
+++ src/app/modules/dashboard.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import {MatSelectionList} from '@angular/material/list';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Propositions} from '../models/Propositions';
 import {TypeQuantity} from '../models/TypeQuantity';
 import {StackedPieChartModel} from '../models/StackedPieChartModel';
 import {BubbleChartInfo} from '../models/BubbleChartInfo';
@@ -78,30 +76,5 @@ export class DashboardService {
 
     return this.httpClient.get<BubbleChartInfo>('http://localhost:8080/propositions/bubble-chart-mandate-inverse');
 
-  }
-
-  // }
-  // tslint:disable-next-line:typedef
-  bigChart() {
-    return [{
-      name: 'Asia',
-      data: [502, 635, 809, 947, 1402, 3634, 5268]
-    }, {
-      name: 'Africa',
-      data: [106, 107, 111, 133, 221, 767, 1766]
-    }, {
-      name: 'Europe',
-      data: [163, 203, 276, 408, 547, 729, 628]
-    }, {
-      name: 'America',
-      data: [18, 31, 54, 156, 339, 818, 1201]
-    }, {
-      name: 'Oceania',
-      data: [2, 2, 2, 6, 13, 30, 46]
-    }];
-  }
-
-  cards(): number[] {
-    return [50, 78, 66, 79];
   }
 }
